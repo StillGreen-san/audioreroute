d5d0fe4aa4b3371fae0938a1ce67749b2497c670
StillGreen-san <40620628+StillGreen-san@users.noreply.github.com>
2023-10-02 18:31:21 +0200

    refactor DetourCreateProcessWithDlls

--- src/exe.cpp
+++ src/exe.cpp
@@ -60,39 +60,20 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
 		return static_cast<int>(GetLastError());
 	}
 
-	//////////////////////////////////////////////////////////////////////////
-	STARTUPINFOA si;
+	STARTUPINFOW si;
 	PROCESS_INFORMATION pi;
-	CHAR szCommand[2048];
-	CHAR szExe[1024];
-	CHAR szFullExe[1024] = "\0";
-	PCHAR pszFileExe = NULL;
-	LPCSTR rpszDllsOut[1] = {dllFullPath.data()};
+	LPCSTR rpszDllsOut = dllFullPath.data();
 
 	ZeroMemory(&si, sizeof(si));
 	ZeroMemory(&pi, sizeof(pi));
 	si.cb = sizeof(si);
 
-	szCommand[0] = L'\0';
+	const DWORD dwFlags = CREATE_DEFAULT_ERROR_MODE | CREATE_SUSPENDED;
 
-	StringCchCopyA(szExe, sizeof(szExe), "C:/Windows/notepad.exe");
-
-	DWORD dwFlags = CREATE_DEFAULT_ERROR_MODE | CREATE_SUSPENDED;
-
-	SetLastError(0);
-	SearchPathA(NULL, szExe, ".exe", ARRAYSIZE(szFullExe), szFullExe, &pszFileExe);
-	if (!DetourCreateProcessWithDllsA(szFullExe, szCommand, NULL, NULL, TRUE, dwFlags, NULL, NULL,
-	       &si, &pi, ARRAYSIZE(rpszDllsOut), rpszDllsOut, NULL)) {
-		DWORD dwError = GetLastError();
-		printf("withdll.exe: DetourCreateProcessWithDllEx failed: %ld\n", dwError);
-		if (dwError == ERROR_INVALID_HANDLE) {
-#if DETOURS_64BIT
-			printf("withdll.exe: Can't detour a 32-bit target process from a 64-bit parent process.\n");
-#else
-			printf("withdll.exe: Can't detour a 64-bit target process from a 32-bit parent process.\n");
-#endif
-		}
-		ExitProcess(9009);
+	if(DetourCreateProcessWithDllsW(exeFullPath.data(), pCmdLine, nullptr, nullptr, TRUE, dwFlags, nullptr, nullptr,
+	       &si, &pi, 1, &rpszDllsOut, nullptr) == FALSE)
+	{
+		return static_cast<int>(GetLastError());
 	}
 
 	ResumeThread(pi.hThread);
